220bf42235e436fe6abe07fb8ce50b8905fd2e3c
M R Kishore Kumar <110018167+kishorekumarcodingmart@users.noreply.github.com>
2022-09-26 12:32:24 +0530

    Delete asset directory

--- asset/javascript/script.js
+++
@@ -1,7 +0,0 @@
-const rightBtn = document.getElementById("rightBtn")
-
-rightBtn.addEventListener("click",() => {
-    let stroy = document.getElementById("story")
-    stroy.scrollBy(30,0)
-    // stroy.style.transitionDuration = "500ms"
-})
