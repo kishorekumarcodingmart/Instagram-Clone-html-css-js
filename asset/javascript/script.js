d961ad5b0088a28d27d9d76534b6529922f132f5
M R Kishore Kumar <110018167+kishorekumarcodingmart@users.noreply.github.com>
2022-09-27 10:11:30 +0530

    JQ

--- asset/javascript/script.js
+++ asset/javascript/script.js
@@ -1,7 +1,12 @@
-const rightBtn = document.getElementById("rightBtn")
+// const rightBtn = document.getElementById("rightBtn")
 
-rightBtn.addEventListener("click",() => {
+// rightBtn.addEventListener("click",() => {
+//     let stroy = document.getElementById("story")
+//     stroy.scrollBy(30,0)
+//     // stroy.style.transitionDuration = "500ms"
+// })
+
+$('#rightBtn').click(function(){
     let stroy = document.getElementById("story")
     stroy.scrollBy(30,0)
-    // stroy.style.transitionDuration = "500ms"
 })
